85f717d2e285e189d3e704827aaca05b93cedd61
= <81452155+YukiUchima@users.noreply.github.com>
2024-03-24 08:36:53 -0700

    finish glab30332

--- assignments/303/GLab30332/ReadingString.java
+++ assignments/303/GLab30332/ReadingString.java
@@ -1,34 +1,34 @@
-package glab30332;
-
 import java.util.Scanner;
+//Guided Lab 303.3.2 - Reading a String From the Console
 
-public class ReadingString {
+public class Glab30332 {
+
+
     public static void main(String[] args) {
-        exampleOne();
-        exampleTwo();
+        Scanner userIn = new Scanner(System.in);
+        exampleOne(userIn);
+        exampleTwo(userIn);
+        userIn.close();
     }
 
-    private static void exampleOne() {
-        Scanner input = new Scanner(System.in);
+    private static void exampleOne(Scanner input) {
         System.out.print("Enter three words separated by spaces: ");
         String s1 = input.next();
         String s2 = input.next();
         String s3 = input.next();
+        input.nextLine();
         System.out.println("s1 is " + s1);
         System.out.println("s2 is " + s2);
         System.out.println("s3 is " + s3);
-        input.close();
     }
 
-    private static void exampleTwo() {
-        Scanner input = new Scanner(System.in);
+    public static void exampleTwo(Scanner input) {
+
         System.out.print("Enter a character: ");
         String s = input.nextLine();
         char ch = s.charAt(0);
         System.out.println("The character entered is " + ch);
 
-        input.close();
     }
-
 
 }
